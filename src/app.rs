56a9f693333a1eeb0b1ed51a2e4a271d0d84ea32
Vishrut Singh <vishrutsingh@Vishruts-MacBook-Air.local>
2023-06-25 13:59:14 +0530

    add structure

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,29 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::views::{home::Home, about::About, contact::Contact};
+
+#[derive(Clone, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[at("/about")]
+    About,
+    #[at("/contact")]
+    Contact,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+pub fn switch (routes: Route) -> Html {
+    match routes {
+        Route::Home => html!{<Home />},
+        Route::About => html!{<About />},
+        Route::Contact => html!{<Contact />},
+        Route::NotFound => html!{<h1>{ "401" }</h1>},
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
---
+++ src/views/mod.rs
@@ -0,0 +1,3 @@
+pub mod home;
+pub mod contact;
+pub mod about;
